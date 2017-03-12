7dadb2cfdb9e99482ef7da1bb3f7bfb4d66d5643
Monish Gupta <monish.gupta1@gmail.com>
2017-03-12 22:34:25 +0530

    Added two cat games each with its score

--- src/js/main.js
+++ src/js/main.js
@@ -1,51 +1,45 @@
 'use strict';
 
 /**
-    If you're writing Cat Clicker with vanilla JS (no jQuery), you'll be adding the "click" event with elem.addEventListener().
-
-    var elem = document.getElementById('my-elem');
-    elem.addEventListener('click', function(){
-    //the element has been clicked... do stuff here
-    }, false);
-    If you're using jQuery, you'll be adding the "click" event listener with jQuery.click().
-
-    $('#my-elem').click(function(e) {
-    //the element has been clicked... do stuff here
-    });
+ * app namespace
  */
 var catApp = catApp || {};
 
+/**
+ * Constants
+ */
 (function(app){
     app.CONSTANTS = app.CONSTANTS || {};
+    app.CONSTANTS.CAT_APP_1_ID_NAME = `cat-app-1`;
+    app.CONSTANTS.CAT_APP_2_ID_NAME = `cat-app-2`;
     app.CONSTANTS.CAT_IMAGE_CLASS_NAME = `cat-app__img`;
     app.CONSTANTS.CAT_SCORE_CLASS_NAME = `cat-app__score`;
 })(catApp);
 
-(function(app){
-    var score = 0;
-    function incScore(){
-        score++;
-    }
-    function getCurrentScore(){
-        return score;
-    }
-    app.scoreManager = {
-        getCurrentScore: getCurrentScore,
-        incScore: incScore
-    };
-})(catApp);
+(function(app, doc) {
+    function Game(domId) {
+        var self = this;
+        this.elGame = doc.querySelector(`#${domId}`);
+        this.elGameImg = this.elGame.querySelector(`.${app.CONSTANTS.CAT_IMAGE_CLASS_NAME}`);
+        this.elGameScore = this.elGame.querySelector(`.${app.CONSTANTS.CAT_SCORE_CLASS_NAME}`);
+        this.score = 0;
 
-(function(app, document){
-    var elAppImg = document.getElementsByClassName(app.CONSTANTS.CAT_IMAGE_CLASS_NAME)[0];
-    var elAppScore = document.getElementsByClassName(app.CONSTANTS.CAT_SCORE_CLASS_NAME)[0];
-
-    function init(){
-        elAppScore.innerText = `Score: 0`;
-        elAppImg.addEventListener('click', function(){
-            app.scoreManager.incScore();
-            var curScore = app.scoreManager.getCurrentScore();
-            elAppScore.innerText = `Score: ${curScore}`;
+        this.elGameImg.addEventListener('click', function(){
+            self.handleClick();
         }, false);
     }
-    init();
+    Game.prototype.handleClick = function(){
+        this.score++;
+        this.elGameScore.innerText = `Score: ${this.score}`;
+    };
+    app.Game = Game;
 })(catApp, document);
+
+
+/**
+ * Game bootstrapping
+ */
+(function(app){
+    var game1 = new app.Game(app.CONSTANTS.CAT_APP_1_ID_NAME);
+    var game2 = new app.Game(app.CONSTANTS.CAT_APP_2_ID_NAME);
+})(catApp);
